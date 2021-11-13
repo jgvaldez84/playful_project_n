780babd8319e08432e1e356b5a1e66265a044372
jgvaldez84 <jgvaldez@madisoncollege.edu>
2021-11-13 15:13:28 -0600

    added functionality to make active note highlighted

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,10 @@
-const Sidebar = ({ notes, onAddNote, onDeleteNote, activeNote, setActiveNote }) => {
+const Sidebar = ({
+  notes,
+  onAddNote,
+  onDeleteNote,
+  activeNote,
+  setActiveNote,
+}) => {
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
@@ -7,17 +13,21 @@ const Sidebar = ({ notes, onAddNote, onDeleteNote, activeNote, setActiveNote }) 
       </div>
       <div className="app-sidebar-notes">
         {notes.map((note) => (
-          <div className="app-sidebar-note" onClick={() => setActiveNote(note.id)}>
+          <div
+            className={`app-sidebar-note ${note.id === activeNote && "active"}`}
+            onClick={() => setActiveNote(note.id)}
+          >
             <div className="sidebar-note-title">
               <strong>{note.title}</strong>
-              <button onClick={()=> onDeleteNote(note.id)}>Delete</button>
+              <button onClick={() => onDeleteNote(note.id)}>Delete</button>
             </div>
             <p>{note.body && note.body.substr(0, 100) + "..."}</p>
             <small className="note-data">
               {" "}
-              Last modified {new Date(note.lastModified).toLocaleDateString("en-US", {
-                  hour: "2-digit",
-                  minute: "2-digit"
+              Last modified{" "}
+              {new Date(note.lastModified).toLocaleDateString("en-US", {
+                hour: "2-digit",
+                minute: "2-digit",
               })}
             </small>
           </div>
