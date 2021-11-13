7565b1ff94c74ac58082b2bd593c7061ea69b811
jgvaldez84 <jgvaldez@madisoncollege.edu>
2021-11-13 14:00:11 -0600

    added header and button for sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Sidebar from './Sidebar';
+import Main from './Main';
+
 
 function App() {
   return (
     <div className="App">
-      Note Pad
+      <Sidebar />
+      <Main />
+      
     </div>
   );
 }
