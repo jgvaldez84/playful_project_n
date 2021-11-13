4231bb06d3e5efa6de2d8e467c9e31f3ded8b618
jgvaldez84 <jgvaldez@madisoncollege.edu>
2021-11-13 14:03:50 -0600

    added more basic divs for buttons on the sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,22 @@
 const Sidebar = () => {
-
-    return <div className="app-sidebar">
-        <div className="app-sidebar-header">
-            <h1>Notes</h1>
-            <button>Add</button>
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>Notes</h1>
+        <button>Add</button>
+      </div>
+      <div className="app-sidebar-notes">
+        <div className="app-sidebar-note">
+          <div className="sidebar-note-title">
+            <strong>Title</strong>
+            <button>Delete</button>
+          </div>
+          <p>Note Preview</p>
+          <small className="note-data"> Last Modified [date]</small>
         </div>
+      </div>
     </div>
-}
+  );
+};
 
 export default Sidebar;
