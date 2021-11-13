8344a5ad50f856341e92c8a1d0fccce8126575ff
jgvaldez84 <jgvaldez@madisoncollege.edu>
2021-11-13 14:43:50 -0600

    imported uuid to generate random id number

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import { useState } from 'react';
+import { v4 as uuid } from "uuid";
 import Sidebar from './Sidebar';
 import Main from './Main';
+
 
 
 function App() {
@@ -9,8 +11,16 @@ function App() {
   const [notes, setNotes] = useState([]);
 
   const onAddNote = () => {
-    console.log('add')
-  }
+    const newNote = {
+      id: uuid(),
+      title: "untitled note",
+      body: "",
+      lastModified: Date.now()
+    };
+
+    setNotes([newNote, ...notes]);
+  };
+
 
   return (
     <div className="App">
