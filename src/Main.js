2db6baa965906434db9a6bbad6ce6de17fbed662
jgvaldez84 <jgvaldez@madisoncollege.edu>
2021-11-13 14:07:47 -0600

    added an input box and a textarea to my main component

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,13 @@
 const Main = () => {
-    return  <p>Main</p>
+    return  <div className="app-main">
+        <div className="app-main-note-edit">
+            <input type="text" id="title" autoFocus />
+            <textarea id="body" placeholder="write your note here"/>
+            <div className="app-main-note-preview">
+            </div>
+            </div>
+
+    </div>
 
 };
 
